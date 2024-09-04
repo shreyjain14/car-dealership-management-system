7f893ee1596500fd771cb5782b85a1021f089708
Dave <dave.sharma002@gmail.com>
2024-09-04 17:14:14 +0530

    Removed

--- customer/customer.h
+++ customer/customer.h
@@ -1,42 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: Remove address, username, password
 
-struct customer {
+
+struct customer
+{
+    int id;
     char name[50];
-    char address[100];
     char phone[10];
     char email[50];
-    char password[50];
-    char username[50];
-    int id;
+    
 };
 
-void addCustomer() {
+void addCustomer()
+{
     struct customer customer;
     printf("Enter name: ");
     scanf("%s", customer.name);
-    printf("Enter address: ");
-    scanf("%s", customer.address);
     printf("Enter phone: ");
     scanf("%s", customer.phone);
     printf("Enter email: ");
     scanf("%s", customer.email);
-    printf("Enter password: ");
-    scanf("%s", customer.password);
-    printf("Enter username: ");
-    scanf("%s", customer.username);
     printf("Enter id: ");
     scanf("%d", &customer.id);
 
     FILE *file = fopen("data/customers.txt", "a");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file.\n");
         return;
     }
 
-    fprintf(file, "%s %s %s %s %s %s %d\n", customer.name, customer.address, customer.phone, customer.email, customer.password, customer.username, customer.id);
+    fprintf(file, "%s %s %s %d\n", customer.name, customer.phone, customer.email, customer.id);
     fclose(file);
 
     printf("Customer added successfully.\n");
