33b3711d720bd86f9637acd30096762cd40d6a65
Shrey <shreyjain1403@gmail.com>
2024-09-04 17:25:56 +0530

    car display fixed

--- car/carDisplay.h
+++ car/carDisplay.h
@@ -4,9 +4,11 @@
 #include <string.h>
 #include "car.h"
 
+#define CAR_FILE_PATH "data/cars.txt"
+
 void displayAllCars() {
 
-    FILE *file = fopen("data/cars.txt", "r");
+    FILE *file = fopen(CAR_FILE_PATH, "r");
     if (file == NULL) {
         printf("Error opening file.\n");
         return;
@@ -14,21 +16,22 @@ void displayAllCars() {
 
     struct Car car;
 
-    printf("\nDEBUG 1\n");
+    printf("Displaying all cars\n");
+    printf("--------------------\n");
 
     while (
         fscanf(
-            file, "%s %s %d %f %s %s %d",
+            file, 
+            "%s %s %d %f %s %s %d",
             car.make, 
             car.model, 
             &car.year, 
             &car.price,
             car.vin, 
             car.engineType, 
-            (int*)&car.isAvailable
-        ) == true) { 
+            (int*)&car.isAvailable) == 7
+                ) { 
 
-        printf("--------------------\n");
         printf("Make: %s\n", car.make);
         printf("Model: %s\n", car.model);
         printf("Year: %d\n", car.year);
@@ -38,50 +41,6 @@ void displayAllCars() {
         printf("Available: %s\n", car.isAvailable ? "Yes" : "No"); 
         printf("--------------------\n");
 
-    }
-
-    fclose(file);
-}
-
-void searchCar(const char* factor, const char* key) {
-    FILE* file = fopen("cars.txt", "r");
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    struct Car car;
-    bool found = false;
-    while (
-        fscanf(
-            file, "%s %s %d %f %s %s %d",
-            car.make, 
-            car.model, 
-            &car.year, 
-            &car.price,
-            car.vin, 
-            car.engineType, 
-            (int*)&car.isAvailable
-        ) == true) { 
-
-        if (strcmp(factor, "VIN") == 0 && strcmp(car.vin, key) == 0) {
-            found = true;
-        } else if (strcmp(factor, "Model") == 0 && strcmp(car.model, key) == 0) {
-            found = true;
-        } // Add more conditions for other factors
-
-        if (found) {
-            printf("--------------------\n");
-            printf("Make: %s\n", car.make);
-            printf("Model: %s\n", car.model);
-            printf("Year: %d\n", car.year);
-            printf("Price: %.2f\n", car.price);
-            printf("VIN: %s\n", car.vin);
-            printf("Engine Type: %s\n", car.engineType);
-            printf("Available: %s\n", car.isAvailable ? "Yes" : "No"); 
-            printf("--------------------\n");
-            found = false;
-        }
     }
 
     fclose(file);
--- car/carQuery.h
+++ car/carQuery.h
@@ -7,34 +7,35 @@ void car_query() {
     int choice;
 
     printf("\n\n-------------------------\n");
-    printf("1. Add Car\n");
-    printf("2. Display all Cars\n");
-    printf("3. Search Car by Make\n");
-    printf("4. Search Car by Model\n");
-    printf("5. Search Car by Year\n");
-    printf("6. Search Car by Price\n");
-    printf("7. Search Car by VIN\n");
-    printf("8. Search Car by Engine Type\n");
-    printf("9. Search Car by Availability\n");
+    printf(" 1. Add Car\n");
+    printf(" 2. Update Car by VIN\n");
+    printf(" 3. Display all Cars\n");
+    printf(" 4. Search Car by Make\n");
+    printf(" 5. Search Car by Model\n");
+    printf(" 6. Search Car by Year\n");
+    printf(" 7. Search Car by Price\n");
+    printf(" 8. Search Car by VIN\n");
+    printf(" 9. Search Car by Engine Type\n");
+    printf("10. Search Car by Availability\n");
     printf("-------------------------\n");
     printf("Enter choice: ");
     scanf("%d", &choice);
 
-    // switch case for car query
+    // TODO switch case for car query
 
     switch (choice)
     {
     case 1:
         addCar();
         break;
-    case 2:
-        printf("Displaying all cars\n");
+    case 3:
         displayAllCars();
         break;
-    case 7:
+    case 8:
         searchCar("VIN", "001");
         break;
     default:
+        printf("Invalid choice.\n");
         break;
     }
 
