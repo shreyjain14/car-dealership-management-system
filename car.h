ef908a14a0941221fcc5acdc6cb56b569ae7f9a4
AnjaneyMitra <mitraanjaney@gmail.com>
2024-09-04 11:27:10 +0530

    added car.h

--- car.h
+++ car.h
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct Car {
     char make[50];
     char model[50];
     int year;
     float price;
+    char vin[20];       
+    char engineType[30]; 
+    bool isAvailable;     
 };
 
 void addCar() {
     struct Car car;
+
     printf("Enter make: ");
-    scanf("%s", car.make);
+    scanf("%s", &car.make);
     printf("Enter model: ");
-    scanf("%s", car.model);
+    scanf("%s", &car.model);
     printf("Enter year: ");
     scanf("%d", &car.year);
     printf("Enter price: ");
     scanf("%f", &car.price);
+    printf("Enter VIN: ");
+    scanf("%s", &car.vin);
+    printf("Enter engine type: ");
+    scanf("%s", &car.engineType);
+    printf("Is the car available? (1 for Yes, 0 for No): ");
+    scanf("%d", &car.isAvailable); 
 
     FILE *file = fopen("cars.txt", "a");
     if (file == NULL) {
@@ -25,7 +36,9 @@ void addCar() {
         return;
     }
 
-    fprintf(file, "%s %s %d %.2f\n", car.make, car.model, car.year, car.price);
+    fprintf(file, "%s %s %d %.2f %s %s %d\n", 
+            car.make, car.model, car.year, car.price,
+            car.vin, car.engineType, car.isAvailable); 
     fclose(file);
 
     printf("Car added successfully.\n");
@@ -39,41 +52,20 @@ void displayCars() {
     }
 
     struct Car car;
-    while (fscanf(file, "%s %s %d %f", car.make, car.model, &car.year, &car.price) != EOF) {
+    while (fscanf(file, "%s %s %d %f %s %s %d", 
+                  car.make, car.model, &car.year, &car.price, 
+                  car.vin, car.engineType, (int*)&car.isAvailable) == true) { 
+
+        printf("--------------------\n");
         printf("Make: %s\n", car.make);
         printf("Model: %s\n", car.model);
         printf("Year: %d\n", car.year);
         printf("Price: %.2f\n", car.price);
+        printf("VIN: %s\n", car.vin);
+        printf("Engine Type: %s\n", car.engineType);
+        printf("Available: %s\n", car.isAvailable ? "Yes" : "No"); 
         printf("--------------------\n");
     }
 
     fclose(file);
-}
-
-int main() {
-    int choice;
-
-    while (1) {
-        printf("Car Dealership Management System\n");
-        printf("1. Add Car\n");
-        printf("2. Display Cars\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                addCar();
-                break;
-            case 2:
-                displayCars();
-                break;
-            case 3:
-                exit(0);
-            default:
-                printf("Invalid choice. Please try again.\n");
-        }
-    }
-
-    return 0;
 }
