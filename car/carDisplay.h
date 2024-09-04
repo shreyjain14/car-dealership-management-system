b8827c281efab0d68d8d383c31657db96a4edb86
Shrey <shreyjain1403@gmail.com>
2024-09-04 18:23:43 +0530

    fixed car searching

--- car/carDisplay.h
+++ car/carDisplay.h
@@ -63,7 +63,11 @@ void displayCar(char *field, char *value) {
     while (fscanf(file, "%s %s %d %f %s %s %d", car.make, car.model, &car.year, &car.price, car.vin, car.engineType, (int*)&car.isAvailable) == 7) {
         if ((strcmp(field, "Make") == 0 && strcmp(car.make, value) == 0) ||
             (strcmp(field, "Model") == 0 && strcmp(car.model, value) == 0) ||
-            (strcmp(field, "Year") == 0 && car.year == atoi(value))) {
+            (strcmp(field, "Year") == 0 && car.year == atoi(value)) ||
+            (strcmp(field, "Price") == 0 && car.price <= atof(value)) ||
+            (strcmp(field, "VIN") == 0 && strcmp(car.vin, value) == 0) ||
+            (strcmp(field, "EngineType") == 0 && strcmp(car.engineType, value) == 0) ||
+            (strcmp(field, "Availability") == 0 && car.isAvailable == (strcmp(value, "1") == 0))) {
             printf("Make: %s\n", car.make);
             printf("Model: %s\n", car.model);
             printf("Year: %d\n", car.year);
--- car/carQuery.h
+++ car/carQuery.h
@@ -6,7 +6,7 @@
 void car_query() {
     int choice;
 
-    printf("\n\n-------------------------\n");
+    printf("\n-------------------------\n");
     printf(" 1. Add Car\n");
     printf(" 2. Update Car by VIN\n");
     printf(" 3. Display all Cars\n");
@@ -31,8 +31,53 @@ void car_query() {
     case 3:
         displayAllCars();
         break;
+    case 4:
+        char make[50];
+        printf("Enter make: ");
+        scanf("%s", make);
+        displayCar("Make", make);
+        break;
+    case 5:
+        char model[50];
+        printf("Enter model: ");
+        scanf("%s", model);
+        displayCar("Model", model);
+        break;
+    case 6:
+        int year;
+        char yearStr[10];
+        printf("Enter year: ");
+        scanf("%d", &year);
+        sprintf(yearStr, "%d", year);
+        displayCar("Year", yearStr);
+        break;
+    case 7:
+        float price;
+        char priceStr[20];
+        printf("Enter price: ");
+        scanf("%f", &price);
+        sprintf(priceStr, "%.2f", price);
+        displayCar("Price", priceStr);
+        break;
     case 8:
-        displayCar("Make", "mazda");
+        char vin[20];
+        printf("Enter VIN: ");
+        scanf("%s", vin);
+        displayCar("VIN", vin);
+        break;
+    case 9:
+        char engineType[30];
+        printf("Enter engine type: ");
+        scanf("%s", engineType);
+        displayCar("Engine Type", engineType);
+        break;
+    case 10:
+        int isAvailable;
+        char availabilityStr[2];
+        printf("Is the car available? (1 for Yes, 0 for No): ");
+        scanf("%d", &isAvailable);
+        sprintf(availabilityStr, "%d", isAvailable);
+        displayCar("Availability", availabilityStr);
         break;
     default:
         printf("Invalid choice.\n");
